b9c271b53e2cf1c5d25cb3ecf335dadda4a48244
maomao <1431608414@qq.com>
2017-12-22 11:20:18 +0800

    Initial Commit

---
+++ HSVideoUDP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HSVideoUDP
+//
+//  Created by MM on 2017/12/22.
+//  Copyright © 2017年 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
